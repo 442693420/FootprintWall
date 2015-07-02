64eb96b9627cc033ff7705ebeb3ea4c15683a6ca
442693420 <442693420@qq.com>
2015-07-02 20:45:13 +0800

    add cr

---
+++ FootprintWall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FootprintWall
+//
+//  Created by 张浩 on 15/4/13.
+//  Copyright (c) 2015年 张浩. All rights reserved.
+//  cr
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
